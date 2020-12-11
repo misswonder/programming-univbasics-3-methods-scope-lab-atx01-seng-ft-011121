6b81933c426e50bbb00c60d665d34a289b2da46e
Yvonne Chen <yvonnechen1991@gmail.com>
2020-12-11 23:24:53 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,6 +1,8 @@
 def mario 
   status = 'Thank You Mario! But Our Princess Is In Another Castle!'
-  puts phrase = "It's-a me, Mario!"
+  phrase = "It's-a me, Mario!"
+  puts phrase
+  phrase 
 end
 mario
 
