bae59413eab4a0ffc2e6f29ad5a302e668d343d9
Yvonne Chen <yvonnechen1991@gmail.com>
2020-12-11 23:47:32 +0000

    Done.

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,8 +1,42 @@
-def mario
-  status = 'Thank You Mario! But Our Princess Is In Another Castle!'
+def mario 
+  phrase = "It's-a me, Mario!"
   puts phrase
+  # phrase 
 end
+mario
 
-def toadstool
+def toadstool 
+  status = 'Thank You Mario! But Our Princess Is In Another Castle!'
   puts status
+  # status
 end
+toadstool
+
+def link 
+  warning = "It's Dangerous To Go Alone! Take This."
+  puts warning
+  # warning
+end
+link 
+
+def all_phrases
+  # phrase = "It's-a me, Mario!"
+  # status = 'Thank You Mario! But Our Princess Is In Another Castle!'
+  # warning = "It's Dangerous To Go Alone! Take This."
+  # puts phrase
+  # puts status
+  # puts warning 
+  #[phrase, status, warning].join("\n")
+  mario
+  toadstool
+  link
+end
+all_phrases
+
+
+
+
+
+  
+
+
