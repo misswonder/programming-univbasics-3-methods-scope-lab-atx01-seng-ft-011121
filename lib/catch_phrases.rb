ee9c9ff5afda2955b696fe2427a3d0d3bb219e64
Yvonne Chen <yvonnechen1991@gmail.com>
2020-12-11 23:32:29 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -13,14 +13,22 @@ end
 tooadstool
 
 def link 
-  warning =  "It's Dangerous To Go Alone! Take This."
+  warning = "It's Dangerous To Go Alone! Take This."
   puts warning
   warning
 end
 link 
 
 def all_phrases
-  
+  phrase = "It's-a me, Mario!"
+  status = 'Thank You Mario! But Our Princess Is In Another'
+  warning = "It's Dangerous To Go Alone! Take This."
+  puts phrase
+  puts status
+  puts warning 
+  phrase, status, warning
+end
+all_phrases
 
 
 
