a81117067fdd256c606e580587aa4ff6aeb52737
Yvonne Chen <yvonnechen1991@gmail.com>
2020-12-11 23:37:05 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -20,13 +20,13 @@ end
 link 
 
 def all_phrases
-  phrase = "It's-a me, Mario!"
-  status = 'Thank You Mario! But Our Princess Is In Another'
-  warning = "It's Dangerous To Go Alone! Take This."
-  puts phrase
-  puts status
-  puts warning 
-  [phrase, status, warning]
+  # phrase = "It's-a me, Mario!"
+  # status = 'Thank You Mario! But Our Princess Is In Another'
+  # warning = "It's Dangerous To Go Alone! Take This."
+  # puts phrase
+  # puts status
+  # puts warning 
+  # [phrase, status, warning]
 end
 all_phrases
 
